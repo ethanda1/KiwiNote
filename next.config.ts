bcbbc8cae22ae1b919b622f6d6536b54483314c2
savii <savii462@gmail.com>
2024-10-28 13:42:44 -0500

    deploy

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   },
-  basePath: '/kiwinote', 
   images: {
     unoptimized: true,} 
 };
