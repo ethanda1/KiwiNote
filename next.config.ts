56288e9e1efbb76f74e8623f590cf6fc681f9ae4
savii <savii462@gmail.com>
2024-10-28 13:16:12 -0500

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: 'export',
   env: {
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  }
+  },
+  basePath: '/kiwinote', 
+  images: {
+    unoptimized: true,} 
 };
 
 export default nextConfig;
